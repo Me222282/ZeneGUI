44b74d39250fe1a13f68336d5da3c49c10887b7f
Zene <oinkymail3@gmail.com>
2022-12-10 18:27:55 +0000

    All functions of ElementManager are now contained in Element

--- src/Element.cs
+++ src/Element.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Zene.Graphics;
 using Zene.Structs;
 using Zene.Windowing;
@@ -15,10 +17,13 @@ namespace Zene.GUI
             Framebuffer.SetColourAttachment(0, TextureFormat.Rgba);
             Framebuffer.SetDepthAttachment(TextureFormat.Depth24Stencil8, false);
         }
-        
-        internal ElementManager _handle;
-        protected ElementManager Manager => _handle;
-        
+
+        internal Window _window;
+        public Element Parent { get; private set; }
+
+        internal virtual TextRenderer textRender => Parent.textRender;
+        public TextRenderer TextRenderer => textRender;
+
         public TextureRenderer Framebuffer { get; }
         public abstract IBasicShader Shader { get; }
         
@@ -36,21 +41,31 @@ namespace Zene.GUI
             {
                 _useLayout = value;
                 
-                if (_handle == null) { return; }
+                if (Parent == null) { return; }
 
                 if (_useLayout)
                 {
-                    _handle.AddLayout(this);
+                    Parent.AddLayout(this);
                     return;
                 }
-                
-                _handle.RemoveLayout(this);
+
+                Parent.RemoveLayout(this);
             }
         }
         
         public Vector2 MouseLocation
         {
             get => _mousePos;
+            set
+            {
+                if (Parent == null)
+                {
+                    _window.MouseLocation = (value.X, -value.Y) + (_window.Size * 0.5);
+                    return;
+                }
+
+                Parent.MouseLocation = value + _bounds.Center;
+            }
         }
         
         public RectangleI Bounds
@@ -80,10 +95,10 @@ namespace Zene.GUI
         }
         
         public bool MouseHover => _mouseOver;
-        
+
+        internal Cursor _currentCursor;
         public Cursor CursorStyle { get; set; }
 
-        private Box _hoverBounds => new Box(_bounds.Center, _bounds.Size + 10d);
         public bool UserResizable { get; internal set; }
 
         public event TextInputEventHandler TextInput;
@@ -99,7 +114,10 @@ namespace Zene.GUI
         public event PositionEventHandler ElementMove;
         
         public event FrameEventHandler Update;
-        
+
+        private readonly object _elementRef = new object();
+        private readonly List<Element> _elements = new List<Element>();
+
         /// <summary>
         /// Determines whether <paramref name="key"/> is currently being pressed.
         /// </summary>
@@ -107,7 +125,7 @@ namespace Zene.GUI
         /// <returns></returns>
         public bool this[Keys key]
         {
-            get => _handle[key];
+            get => _window[key];
         }
         /// <summary>
         /// Determines whether <paramref name="mod"/> is currently active.
@@ -120,79 +138,162 @@ namespace Zene.GUI
         /// <returns></returns>
         public bool this[Mods mod]
         {
-            get => _handle[mod];
+            get => _window[mod];
         }
-        
-        internal void Render(IFramebuffer framebuffer, IBasicShader shader, IDrawable draw)
+
+        public void AddElement(Element e)
         {
+            if (e.Parent != null)
+            {
+                throw new ArgumentException("Element already managed.", nameof(e));
+            }
+
+            e.Parent = this;
+            if (_window != null)
+            {
+                e._window = _window;
+
+                // Set child elements
+                lock (e._elementRef)
+                {
+                    Span<Element> span = CollectionsMarshal.AsSpan(e._elements);
+
+                    for (int i = 0; i < span.Length; i++)
+                    {
+                        span[i]._window = _window;
+                    }
+                }
+            }
+
+            lock (_elementRef)
+            {
+                _elements.Add(e);
+            }
+
+            if (e.UseLayout) { AddLayout(e); }
+
+            if (_window != null && _window.Running) { e.OnStart(); }
+        }
+
+        internal void Render(IFramebuffer framebuffer, Matrix4 projection, IDrawable draw)
+        {
+            if (Shader == null) { return; }
+
             Framebuffer.Bind();
             Shader.Bind();
-            
+
+            Shader.SetMatrices(
+                Matrix4.Identity,
+                Matrix4.Identity,
+                Projection);
+
             OnUpdate(new FrameEventArgs(Framebuffer, Shader));
 
+            // Draw child elements
+            lock (_elementRef)
+            {
+                Span<Element> span = CollectionsMarshal.AsSpan(_elements);
+
+                for (int i = 0; i < span.Length; i++)
+                {
+                    textRender.Projection = span[i].Projection;
+
+                    span[i].Render(Framebuffer, Projection, draw);
+                }
+            }
+
             framebuffer.Bind();
-            shader.Bind();
-            shader.Matrix1 = Matrix4.CreateBox(_bounds);
-            shader.ColourSource = ColourSource.Texture;
-            shader.TextureSlot = 0;
+            Shader.Bind();
+            Shader.SetMatrices(
+                Matrix4.CreateBox(_bounds),
+                Matrix4.Identity,
+                projection);
+            Shader.ColourSource = ColourSource.Texture;
+            Shader.TextureSlot = 0;
             Framebuffer.GetTexture(FrameAttachment.Colour0).Bind();
             draw.Draw();
         }
         
         internal void OnStart()
         {
-            OnSizeChange(new SizeChangeEventArgs(_bounds.Size));
-            OnElementMove(new PositionEventArgs(_bounds.Location));
-        }
+            RectangleI rect = _bounds;
+            _bounds = new RectangleI();
 
-        private bool _mouseOver = false;
-        internal MouseMange ManageMouse(Vector2 location)
-        {
-            bool mouseOverNew = _bounds.Contains(location);
+            OnSizeChange(new SizeChangeEventArgs(rect.Size));
+            OnElementMove(new PositionEventArgs(rect.Location));
 
-            if ((mouseOverNew == _mouseOver) && (_mouseOver == false)) { return MouseMange.NoMouse; }
-
-            // Must also equal true
-            if (mouseOverNew == _mouseOver)
+            // Trigger first OnSizeChange for child elements
+            lock (_elementRef)
             {
-                OnMouseMove(new MouseEventArgs(location - _bounds.Center));
-                return MouseMange.MouseOver;
-            }
-            
-            if (!mouseOverNew)
-            {
-                _mouseOver = false;
-                OnMouseLeave(new EventArgs());
-                return MouseMange.MouseLeave;
-            }
-            
-            _mouseOver = true;
-            OnMouseEnter(new EventArgs());
-            OnMouseMove(new MouseEventArgs(location - Bounds.Center));
-            return MouseMange.MouseEnter;
-        }
-        
-        private void CheckForResize()
-        {
+                Span<Element> span = CollectionsMarshal.AsSpan(_elements);
 
+                for (int i = 0; i < span.Length; i++)
+                {
+                    span[i].OnStart();
+                }
+            }
         }
 
-        protected internal virtual void OnTextInput(TextInputEventArgs e)
+        protected virtual void OnTextInput(TextInputEventArgs e)
         {
             TextInput?.Invoke(this, e);
+
+            // Update child elements
+            lock (_elementRef)
+            {
+                Span<Element> span = CollectionsMarshal.AsSpan(_elements);
+
+                for (int i = 0; i < span.Length; i++)
+                {
+                    span[i].OnTextInput(e);
+                }
+            }
         }
-        protected internal virtual void OnKeyDown(KeyEventArgs e)
+        protected virtual void OnKeyDown(KeyEventArgs e)
         {
             KeyDown?.Invoke(this, e);
+
+            // Update child elements
+            lock (_elementRef)
+            {
+                Span<Element> span = CollectionsMarshal.AsSpan(_elements);
+
+                for (int i = 0; i < span.Length; i++)
+                {
+                    span[i].OnKeyDown(e);
+                }
+            }
         }
-        protected internal void OnKeyUp(KeyEventArgs e)
+        protected virtual void OnKeyUp(KeyEventArgs e)
         {
             KeyUp?.Invoke(this, e);
+
+            // Update child elements
+            lock (_elementRef)
+            {
+                Span<Element> span = CollectionsMarshal.AsSpan(_elements);
+
+                for (int i = 0; i < span.Length; i++)
+                {
+                    span[i].OnKeyUp(e);
+                }
+            }
         }
-        protected internal virtual void OnScroll(ScrollEventArgs e)
+
+        protected virtual void OnScroll(ScrollEventArgs e)
         {
             Scroll?.Invoke(this, e);
+
+            // Update child elements
+            Span<Element> span = CollectionsMarshal.AsSpan(_hover);
+
+            for (int i = 0; i < span.Length; i++)
+            {
+                span[i].OnScroll(e);
+            }
         }
+
+        private readonly List<Element> _hover = new List<Element>();
         protected virtual void OnMouseEnter(EventArgs e)
         {
             MouseEnter?.Invoke(this, e);
@@ -203,44 +304,147 @@ namespace Zene.GUI
         }
         protected virtual void OnMouseMove(MouseEventArgs e)
         {
-            if (UserResizable)
-            {
-                CheckForResize();
-            }
-
             lock (_mousePosRef)
             {
+                if (_mousePos == e.Location) { return; }
+
                 _mousePos = e.Location;
             }
             MouseMove?.Invoke(this, e);
+
+            lock (_elementRef)
+            {
+                Cursor newCursor = CursorStyle;
+
+                Span<Element> span = CollectionsMarshal.AsSpan(_elements);
+
+                for (int i = 0; i < span.Length; i++)
+                {
+                    bool mouseOverNew = span[i]._bounds.Contains(e.Location);
+
+                    // Mouse not in bounds
+                    if ((mouseOverNew == span[i]._mouseOver) && (span[i]._mouseOver == false)) { continue; }
+                    // mouseOverNew will also equal true
+                    // Mouse moves inside bounds
+                    if (mouseOverNew == span[i]._mouseOver)
+                    {
+                        span[i].OnMouseMove(new MouseEventArgs(e.Location - span[i]._bounds.Center));
+                        newCursor = span[i]._currentCursor;
+                        continue;
+                    }
+
+                    // Mouse enters bounds
+                    if (mouseOverNew)
+                    {
+                        span[i]._mouseOver = true;
+                        span[i].OnMouseEnter(new EventArgs());
+                        span[i].OnMouseMove(new MouseEventArgs(e.Location - span[i]._bounds.Center));
+
+                        _hover.Add(span[i]);
+                        newCursor = span[i]._currentCursor;
+                        continue;
+                    }
+
+                    // Mouse leaves bounds
+                    span[i]._mouseOver = false;
+                    span[i].OnMouseLeave(new EventArgs());
+                    _hover.Remove(span[i]);
+                }
+
+                _currentCursor = newCursor;
+            }
         }
-        protected internal virtual void OnMouseDown(MouseEventArgs e)
+        private bool _mouseOver = false;
+        protected virtual void OnMouseDown(MouseEventArgs e)
         {
             MouseDown?.Invoke(this, e);
+
+            // Update child elements
+            Span<Element> span = CollectionsMarshal.AsSpan(_hover);
+
+            for (int i = 0; i < span.Length; i++)
+            {
+                span[i].OnMouseDown(e);
+            }
         }
-        protected internal virtual void OnMouseUp(MouseEventArgs e)
+        protected virtual void OnMouseUp(MouseEventArgs e)
         {
             MouseUp?.Invoke(this, e);
+
+            // Update child elements
+            Span<Element> span = CollectionsMarshal.AsSpan(_hover);
+
+            for (int i = 0; i < span.Length; i++)
+            {
+                span[i].OnMouseUp(e);
+            }
         }
+
+        internal void AddLayout(Element e)
+        {
+            lock (_elLayoutRef)
+            {
+                _layouts.Add(e);
+            }
+
+            Vector2 multiplier = _bounds.Size * 0.5;
+
+            Vector2 tl = (e.Layout.Left, e.Layout.Top);
+
+            e.Bounds = new RectangleI(
+                tl * multiplier,
+                e.Layout.Size * multiplier
+            );
+        }
+        internal void RemoveLayout(Element e)
+        {
+            lock (_elLayoutRef)
+            {
+                _layouts.Remove(e);
+            }
+        }
+
+        private readonly object _elLayoutRef = new object();
+        private readonly List<Element> _layouts = new List<Element>();
+
+        public Matrix4 Projection { get; private set; }
         protected virtual void OnSizeChange(SizeChangeEventArgs e)
         {
             lock (_boundsRef)
             {
+                //if (_bounds.Size == e.Size) { return; }
+
                 _bounds.Size = e.Size;
             }
             Framebuffer.ViewSize = e.Size;
             Framebuffer.Size = e.Size;
-            if (Shader != null)
-            {
-                Shader.Matrix3 = Matrix4.CreateOrthographic(e.Width, e.Height, 0d, 1d);
-            }
+            Projection = Matrix4.CreateOrthographic(e.Width, e.Height, 0d, 1d);
             
             SizeChange?.Invoke(this, e);
+
+            // Update child elements
+            lock (_elLayoutRef)
+            {
+                Vector2 multiplier = e.Size * 0.5;
+                Span<Element> span = CollectionsMarshal.AsSpan(_layouts);
+
+                for (int i = 0; i < span.Length; i++)
+                {
+                    Vector2 tl = (span[i].Layout.Left, span[i].Layout.Top);
+
+                    span[i].Bounds = new RectangleI(
+                        tl * multiplier,
+                        span[i].Layout.Size * multiplier
+                    );
+                }
+            }
         }
         protected virtual void OnElementMove(PositionEventArgs e)
         {
             lock (_boundsRef)
             {
+                if (_bounds.Location == e.Location) { return; }
+
                 _bounds.Location = e.Location;
             }
             ElementMove?.Invoke(this, e);
