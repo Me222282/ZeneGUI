218b4802218b134cd36c1e53f6dd18a3296ae2c4
Zene <oinkymail3@gmail.com>
2022-12-28 19:32:37 +0000

    Added MultiLayout.cs and MenuLayout.cs

---
+++ src/Layouts/MenuLayout.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Zene.Structs;
+
+namespace Zene.GUI.Layouts
+{
+    public class MenuLayout : ILayout
+    {
+        public MenuLayout(double height)
+        {
+            _height = height;
+        }
+
+        private double _height;
+        public double Height
+        {
+            get => _height;
+            set
+            {
+                if (_height == value) { return; }
+
+                _height = value;
+                Change?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public event EventHandler Change;
+
+        public Box GetBounds(LayoutArgs args)
+        {
+            double top = args.Size.Y * 0.5;
+
+            return new Box(
+                args.Size.X * -0.5,
+                args.Size.X * 0.5,
+                top,
+                top - _height);
+        }
+    }
+}
---
+++ src/Layouts/MultiLayout.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Zene.Structs;
+
+namespace Zene.GUI.Layouts
+{
+    public class MultiLayout : ILayout
+    {
+        public MultiLayout(ILayout location, ILayout size)
+        {
+            Location = location;
+            Size = size;
+        }
+
+        private ILayout _location;
+        /// <summary>
+        /// The layout used to calculate location.
+        /// </summary>
+        public ILayout Location
+        {
+            get => _location;
+            set
+            {
+                if (_location == value) { return; }
+
+                if (_location != null)
+                {
+                    _location.Change -= ChangeInvoke;
+                }
+
+                _location = value;
+
+                if (_location == null) { return; }
+                _location.Change += ChangeInvoke;
+            }
+        }
+
+        private ILayout _size;
+        /// <summary>
+        /// The layout used to calculate size.
+        /// </summary>
+        public ILayout Size
+        {
+            get => _size;
+            set
+            {
+                if (_size == value) { return; }
+
+                if (_size != null)
+                {
+                    _size.Change -= ChangeInvoke;
+                }
+
+                _size = value;
+
+                if (_size == value) { return; }
+                _size.Change += ChangeInvoke;
+            }
+        }
+
+        public event EventHandler Change;
+
+        private void ChangeInvoke(object sender, EventArgs e) => Change?.Invoke(sender, e);
+
+        public Box GetBounds(LayoutArgs args)
+        {
+            Box location = default;
+            if (_location != null)
+            {
+                location = _location.GetBounds(args);
+            }
+
+            Box size = default;
+            if (_size != null)
+            {
+                size = _size.GetBounds(args);
+            }
+
+            return new Box(location.Location, size.Size);
+        }
+    }
+}
