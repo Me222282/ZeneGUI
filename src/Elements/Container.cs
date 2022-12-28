93aac9d5feadcaa2dba46a1f619abd513b46f8f9
Zene <oinkymail3@gmail.com>
2022-12-28 19:39:12 +0000

    Fixed namespace for new Layouts
    Container.cs can now have a background colour.

--- src/Elements/Container.cs
+++ src/Elements/Container.cs
@@ -1,4 +1,5 @@
-﻿using Zene.Structs;
+﻿using Zene.Graphics;
+using Zene.Structs;
 
 namespace Zene.GUI
 {
@@ -14,6 +15,27 @@ namespace Zene.GUI
         public Container(ILayout layout)
             : base(layout)
         {
+        }
+
+        private readonly BasicShader _shader = BasicShader.GetInstance();
+        public ColourF Colour { get; set; }
+
+        protected override void OnUpdate(FrameEventArgs e)
+        {
+            base.OnUpdate(e);
+
+            // No colour
+            if (Colour.A <= 0f) { return; }
+
+            _shader.Bind();
+
+            _shader.ColourSource = ColourSource.UniformColour;
+            _shader.Colour = Colour;
+            _shader.Matrix1 = Matrix4.CreateScale(2d);
+            _shader.Matrix2 = Matrix4.Identity;
+            _shader.Matrix3 = Matrix4.Identity;
+
+            Shapes.Square.Draw();
         }
     }
 }
--- src/Layouts/MenuLayout.cs
+++ src/Layouts/MenuLayout.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Zene.Structs;
 
-namespace Zene.GUI.Layouts
+namespace Zene.GUI
 {
     public class MenuLayout : ILayout
     {
