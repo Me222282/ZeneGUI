6518445624bc103434c4881a6390b226e33c7228
Zene <oinkymail3@gmail.com>
2023-01-23 15:39:57 +0000

    Added LayoutManager<T> for targeted child element types

---
+++ src/LayoutManagers/LayoutManagerT.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Zene.Structs;
+
+namespace Zene.GUI
+{
+    public abstract class LayoutManager<T> : LayoutManager where T : Element
+    {
+        /// <summary>
+        /// Determines whether an exception should be thrown if child element is not of type <see cref="T"/>.
+        /// </summary>
+        protected bool ThrowException { get; set; } = true;
+
+        /// <summary>
+        /// Called once for every element.
+        /// </summary>
+        /// <param name="element"></param>
+        /// <param name="args"></param>
+        /// <param name="layoutResult"></param>
+        /// <returns></returns>
+        protected abstract Box GetBounds(T element, LayoutArgs args, Box layoutResult);
+
+        protected override Box GetBounds(LayoutArgs args, Box layoutResult)
+        {
+            if (args.Element is not T e)
+            {
+                if (!ThrowException)
+                {
+                    return layoutResult;
+                }
+
+                throw new ArgumentException(nameof(args.Element));
+            }
+
+            return GetBounds(e, args,layoutResult);
+        }
+    }
+}
